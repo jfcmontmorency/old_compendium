5358fa5ea3c92c344a0fd3b6af42089a1476c77f
Jean-François Cartier <142059735+jfcmontmorency@users.noreply.github.com>
2024-03-05 15:30:30 -0500

    Update extra.js

--- docs/_/extra.js
+++ docs/_/extra.js
@@ -1,2 +1,12 @@
-// Supprime les tabs
+// Retire la navigation principale sous forme de tabs 
 document.querySelector('.md-container > .md-tabs').outerHTML = '';
+
+// Retrait du lien sur le logo
+var wrapper = document.querySelector('.md-header__button.md-logo');
+if (wrapper) {
+    var docFrag = document.createDocumentFragment();
+    while (wrapper.firstChild) {
+        docFrag.appendChild(wrapper.firstChild);
+    }
+    wrapper.parentNode.replaceChild(docFrag, wrapper);
+}
